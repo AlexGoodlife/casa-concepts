a1503498f52447b3670e470db60cbec72ad96c7a
AlexGoodlife <novaisboavida@gmail.com>
2024-11-22 18:57:33 +0000

    maybe?

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
-  site: 'https://AlexGoodlife.github.io',
+  site: 'https://alexgoodlife.github.io',
 
   integrations: [
     tailwind({
